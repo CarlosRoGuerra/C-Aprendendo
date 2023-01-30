a5e4c8c87201222a61114dc7a9344256b2fbd8fe
Carlos Roberto <crrobg2005@yahoo.com.br>
2023-01-30 17:13:01 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Globalization;
+namespace Curso
+{
+    class Program{
+        static void Main(string[] args) {
+            string Produto1 = "computador";
+            string Produto2 = "Mesa de escritório";
+
+            byte idade =3;
+            int codigo = 5290;
+            char genero = 'M';
+            
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double media = 52.234567;
+            
+            System.Console.WriteLine($"{Produto1}, cujo o preco é {preco1}");
+            System.Console.WriteLine($"{Produto2}, cujo o preco é {preco2}");
+
+            System.Console.WriteLine($"{idade} anos de idade, codigo {codigo} e genero {genero}");
+
+            System.Console.WriteLine($"{media:F8}");
+            System.Console.WriteLine($"{media:F3}");
+            System.Console.WriteLine(media.ToString("F3",CultureInfo.InvariantCulture));
+
+            //Exemplos
+            //System.Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F1} reais", nome, idade, saldo);//PLaceHoders
+            //System.Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2}reais");//interpolação
+            //System.Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F1")+ " reais");//concatenação
+        }
+    }
+}
---
+++ downsleeping.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+# This script will put the computer to sleep after 90 minutes
+
+# This script is intended to be run from the command line, not from a desktop icon
+
+shutdown -s -t 5400
